9bb65c4676093256f45b85783d70a894b48800f7
Rahil khan <65817230+ErrorxCode@users.noreply.github.com>
2022-06-10 22:13:09 +0530

    fixed database endpoint

--- Clorabase/src/main/java/com/clorabase/Constants.java
+++ Clorabase/src/main/java/com/clorabase/Constants.java
@@ -3,5 +3,5 @@ package com.clorabase;
 public class Constants {
     public static final String CLIENT_ID = "402416439097-j01jvkbrkjttqb1ugoopi4hu7bi94a3o.apps.googleusercontent.com";
     public static final String CLIENT_SECRET = "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";
-    public static final String DATABASE_BASE_URL = "wss://clorabase.herokuapp.com/database";
+    public static final String DATABASE_BASE_URL = "wss://clorabase.herokuapp.com/clorem";
 }
