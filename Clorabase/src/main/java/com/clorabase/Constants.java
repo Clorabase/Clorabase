9f4a40b338cab060651712b4cde5b2f106fc5e9a
RAHIL <65817230+ErrorxCode@users.noreply.github.com>
2022-08-21 01:08:09 +0530

    Delete Clorabase directory

--- Clorabase/src/main/java/com/clorabase/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.clorabase;
-
-public class Constants {
-    public static final String CLIENT_ID = "402416439097-j01jvkbrkjttqb1ugoopi4hu7bi94a3o.apps.googleusercontent.com";
-    public static final String CLIENT_SECRET = "********************";
-    public static final String DATABASE_BASE_URL = "wss://clorabase.herokuapp.com/clorem";
-}
