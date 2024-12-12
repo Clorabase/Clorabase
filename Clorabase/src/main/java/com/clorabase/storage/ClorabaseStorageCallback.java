a74774696bde9090ee2a00d84b45cacba2a57bce
Rahil khan <65817230+ErrorxCode@users.noreply.github.com>
2024-12-12 15:05:15 +0530

    Outdated version

--- Clorabase/src/main/java/com/clorabase/storage/ClorabaseStorageCallback.java
+++
@@ -1,9 +0,0 @@
-package com.clorabase.storage;
-
-import androidx.annotation.NonNull;
-
-public interface ClorabaseStorageCallback {
-    void onFailed(@NonNull Exception e);
-    void onProgress(int percent);
-    void onComplete();
-}
