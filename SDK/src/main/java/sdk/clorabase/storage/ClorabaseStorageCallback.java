8a0ff42c5f1466dc9f5503e0cfeb90ccfed260cb
Rahil <65817230+ErrorxCode@users.noreply.github.com>
2025-09-11 00:06:55 +0530

    Delete lagacy SDK

--- SDK/src/main/java/sdk/clorabase/storage/ClorabaseStorageCallback.java
+++
@@ -1,9 +0,0 @@
-package sdk.clorabase.storage;
-
-import androidx.annotation.NonNull;
-
-public interface ClorabaseStorageCallback {
-    void onFailed(@NonNull Exception e);
-    void onProgress(int percent);
-    void onComplete();
-}
